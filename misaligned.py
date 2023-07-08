6d1af061d9d4fc1766e131c2aa051aad8db96b78
Ritunjay-11 <66157748+Ritunjay-11@users.noreply.github.com>
2023-07-08 23:01:43 +0530

    Update misaligned.py
    2d and 6s are width specifiers to ensure properly aligned output
    concerns and alignment both are handled by different functions, which ensures string formatting is looked into

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,20 @@
 
-def print_color_map():
+def generate_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    color_map = []
+
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            color_index = i * 5 + j
+            color_pair = (color_index, major, minor)
+            color_map.append(color_pair)
 
+    return color_map
 
-result = print_color_map()
-assert(result == 25)
-print("All is well (maybe!)\n")
+def print_color_map(color_map):
+    for color_pair in color_map:
+        print(f'{color_pair[0]:2d} | {color_pair[1]:6s} | {color_pair[2]:6s}')
+
+color_map = generate_color_map()
+print_color_map(color_map)
